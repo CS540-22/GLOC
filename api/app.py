85bd1f5571354d00ff60c738bba7e9ea05bd95f2
bgreenb11 <blg1111@outlook.com>
2022-02-27 15:06:10 -0500

    Begin test api for cloc under  /api

---
+++ api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_executor import Executor
+from flask_shell2http import Shell2HTTP
+
+app = Flask(__name__)
+
+executor = Executor(app)
+shell2http = Shell2HTTP(app=app, executor=executor, base_url_prefix="/")
+
+
+def callback(context, future):
+    print(context, future.result())
+
+
+shell2http.register_command(
+    endpoint="ls", command_name="ls", callback_fn=callback, decorators=[])
