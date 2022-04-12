fde67923d2a86ea2459c2411ff52f46dc77555fe
daniel-troutman <dtroutm1@vols.utk.edu>
2022-04-11 23:35:14 -0400

    Add more data files

--- gloc_ui/lib/data/ClocRequest.dart
+++ gloc_ui/lib/data/ClocRequest.dart
@@ -1,13 +1,19 @@
+import 'package:http/http.dart';
+
+enum RequestType { single, history }
+
 class ClocRequest {
   final Uri giturl;
+  final RequestType type;
 
-  ClocRequest(String url) : giturl = Uri.parse(url);
+  ClocRequest(String url, this.type) : giturl = Uri.parse(url);
 
-  Uri generateRequestURL() {
-    final queryParameters = {
-      'giturl': '$giturl',
-    };
-
-    return Uri.https('gloc.homelab.benlg.dev', 'gloc', queryParameters);
+  Future<Response> sendRequest() async {
+    return await post(
+      Uri.https('gloc.homelab.benlg.dev', type.name),
+      body: <String, String>{
+        'url': '$giturl',
+      },
+    );
   }
 }
