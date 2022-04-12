693bc52f8d8826deda104471a0984312c951394a
bgreenb11 <blg1111@outlook.com>
2022-04-12 18:51:22 -0400

    Added deployment setup for gloc ui

--- cluster/docker/gloc_ui/build.sh
+++ cluster/docker/gloc_ui/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f ./cluster/docker/gloc_ui/Dockerfile -t bgreenb11/gloc-ui:latest . 
-docker push bgreenb11/gloc-ui:latest
+docker build -f ./cluster/docker/gloc_ui/Dockerfile -t bgreenb11/gloc-ui:arm . 
+docker push bgreenb11/gloc-ui:arm
