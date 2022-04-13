0fefd7fad4be28ac3402dbd6c36151b1cc5fb7db
Spencer Howell <spencerhowell98@gmail.com>
2022-04-12 23:13:50 -0400

    Move horizontal bar graph to reusable widget

--- gloc_ui/lib/widgets/DetailsPage.dart
+++ gloc_ui/lib/widgets/DetailsPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:gloc_ui/data/ClocResult.dart';
+import 'package:collection/collection.dart';
 
 import '../data/LanguageResult.dart';
 import 'LangaugePieChart.dart';
@@ -15,27 +17,24 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(maxWidth: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _RepoTitle(url: clocResult.giturl),
-          SizedBox(height: _cardSpacing),
-          _CodeCount(
-              totalLines: clocResult.totalLines,
-              totalCode: clocResult.totalCode,
-              totalBlank: clocResult.totalBlank,
-              totalComment: clocResult.totalComment),
-          SizedBox(height: _cardSpacing * 2),
-          // Double spacer to avoid overlapping
-          LanguagePieChart(
-            clocResult: clocResult,
-          ),
-          SizedBox(height: _cardSpacing * 2),
-          Expanded(child: _LanguageCardList(clocResult.languages)),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _RepoTitle(url: clocResult.giturl),
+        SizedBox(height: _cardSpacing),
+        _CodeCount(
+            totalLines: clocResult.totalLines,
+            totalCode: clocResult.totalCode,
+            totalBlank: clocResult.totalBlank,
+            totalComment: clocResult.totalComment),
+        SizedBox(height: _cardSpacing * 2),
+        // Double spacer to avoid overlapping
+        LanguagePieChart(
+          clocResult: clocResult,
+        ),
+        SizedBox(height: _cardSpacing * 2),
+        Expanded(child: _LanguageCardList(clocResult.languages)),
+      ],
     );
   }
 }
@@ -88,16 +87,21 @@ class _CodeCount extends StatelessWidget {
       children: [
         Text(
           '$totalLines lines of code',
-          style: Theme.of(context).textTheme.titleSmall,
+          style: Theme.of(context).textTheme.titleMedium,
         ),
-        ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 400),
-          child: Row(children: [
-            _buildBarSection(totalCode, Colors.red),
-            _buildBarSection(totalComment, Colors.blue),
-            _buildBarSection(totalBlank, Colors.green),
-          ]),
-        ),
+        _HorizontalBarGraph([
+          totalLines,
+          totalComment,
+          totalBlank
+        ], [
+          Theme.of(context).primaryColor,
+          Theme.of(context).primaryColor.withAlpha(80),
+          Theme.of(context).disabledColor
+        ], [
+          'Code',
+          'Comment',
+          'Blank'
+        ])
       ],
     );
   }
@@ -115,11 +119,16 @@ class _LanguageCardList extends StatelessWidget {
       itemBuilder: (BuildContext context, int index) {
         final data = languages[index];
         final totalLines = data.blank + data.code + data.comment;
+        final icon = SvgPicture.asset(
+          data.icon.path,
+          width: 56.0,
+          height: 56.0,
+        );
 
         return Card(
             child: Column(children: [
           ListTile(
-            leading: FlutterLogo(), //TODO replace with language icon
+            leading: icon,
             title: Text(
               data.name,
               style: Theme.of(context).textTheme.headlineMedium,
@@ -131,6 +140,62 @@ class _LanguageCardList extends StatelessWidget {
           )
         ]));
       },
+    );
+  }
+}
+
+class _HorizontalBarGraph extends StatelessWidget {
+  const _HorizontalBarGraph(this.values, this.colors, this.labels);
+
+  final List<int> values;
+  final List<Color> colors;
+  final List<String> labels;
+  final double barHeight = 10.0;
+
+  Widget _buildBarSection(int count, Color color) {
+    return Expanded(
+      flex: ((count / values.sum) * 100.0).round(),
+      child: Container(
+        height: barHeight,
+        color: color,
+      ),
+    );
+  }
+
+  Widget _buildLabel(BuildContext context, String text, Color color) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          width: 10.0,
+          height: 10.0,
+          decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+        ),
+        SizedBox(width: 5.0),
+        Text(text, style: Theme.of(context).textTheme.titleSmall)
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sections = <Widget>[];
+    final legend = <Widget>[];
+    values.forEachIndexed((index, value) {
+      sections.add(_buildBarSection(value, colors[index]));
+      legend.add(_buildLabel(context, labels[index], colors[index]));
+    });
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 400),
+      child: Column(children: [
+        Row(children: sections),
+        SizedBox(height: 5.0),
+        Row(
+          children: legend,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        ),
+      ]),
     );
   }
 }
