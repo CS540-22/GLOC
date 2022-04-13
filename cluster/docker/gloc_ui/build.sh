3ca3aa11a5745e5240d3c6a3ad555075019ef0f4
bgreenb11 <blg1111@outlook.com>
2022-04-12 22:26:22 -0400

    Made changes to support frontend integration

--- cluster/docker/gloc_ui/build.sh
+++ cluster/docker/gloc_ui/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-docker build -f ./cluster/docker/gloc_ui/Dockerfile -t bgreenb11/gloc-ui:arm . 
+source ./gloc_ui/.env
+docker build --build-arg api_url=$API_URL -f ./cluster/docker/gloc_ui/Dockerfile -t bgreenb11/gloc-ui:arm . 
 docker push bgreenb11/gloc-ui:arm
