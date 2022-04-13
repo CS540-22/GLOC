af10f1aa1b37d8c76dad602449c5f73561385296
daniel-troutman <dtroutm1@vols.utk.edu>
2022-04-12 21:07:20 -0400

    Add pie chart

--- gloc_ui/lib/widgets/DetailsPage.dart
+++ gloc_ui/lib/widgets/DetailsPage.dart
@@ -1,5 +1,9 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_palette/flutter_palette.dart';
 import 'package:gloc_ui/data/ClocResult.dart';
+import 'package:gloc_ui/data/LanguageIcon.dart';
+import 'package:gloc_ui/data/LanguageResult.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({
@@ -14,20 +18,22 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(maxWidth: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-
-          children: [
-            //TODO use repo name and URL once added to the result data
-            _RepoTitle(url: "github.com/username/MyExampleRepository"),
-            SizedBox(height: _cardSpacing),
-            _CodeCount(
-                totalLines: clocResult.totalLines,
-                totalCode: clocResult.totalCode,
-                totalBlank: clocResult.totalBlank,
-                totalComment: clocResult.totalComment),
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          //TODO use repo name and URL once added to the result data
+          _RepoTitle(url: "github.com/username/MyExampleRepository"),
+          SizedBox(height: _cardSpacing),
+          _CodeCount(
+              totalLines: clocResult.totalLines,
+              totalCode: clocResult.totalCode,
+              totalBlank: clocResult.totalBlank,
+              totalComment: clocResult.totalComment),
+          _LanguagePieChart(
+            clocResult: clocResult,
+          ),
+        ],
+      ),
     );
   }
 }
@@ -88,12 +94,127 @@ class _CodeCount extends StatelessWidget {
         ),
         Row(children: [
           _buildBarSection(totalCode, Colors.red),
-          _buildBarSection(
-              totalComment, Colors.blue),
+          _buildBarSection(totalComment, Colors.blue),
           _buildBarSection(totalBlank, Colors.green),
         ]),
         Text('Hello'),
       ],
     );
+  }
+}
+
+class _LanguagePieChart extends StatefulWidget {
+  const _LanguagePieChart({
+    Key? key,
+    required this.clocResult,
+  }) : super(key: key);
+
+  final ClocResult clocResult;
+  @override
+  _LanguagePieChartState createState() => _LanguagePieChartState();
+}
+
+class _LanguagePieChartState extends State<_LanguagePieChart> {
+  int touchedIndex = -1;
+  late List<LanguageResult> graphLanguages;
+  late ColorPalette graphColors;
+
+  @override
+  void initState() {
+    super.initState();
+    graphLanguages = generateGraphLanguages(widget.clocResult, .05);
+    graphColors = ColorPalette.polyad(
+      const HslColor(300, 100, 40),
+      numberOfColors: graphLanguages.length,
+      // hueVariability: 15,
+      // saturationVariability: 10,
+      // brightnessVariability: 10,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: PieChart(
+        PieChartData(
+            pieTouchData: PieTouchData(
+                touchCallback: (FlTouchEvent event, pieTouchResponse) {
+              setState(() {
+                if (!event.isInterestedForInteractions ||
+                    pieTouchResponse == null ||
+                    pieTouchResponse.touchedSection == null) {
+                  touchedIndex = -1;
+                  return;
+                }
+                touchedIndex =
+                    pieTouchResponse.touchedSection!.touchedSectionIndex;
+              });
+            }),
+            borderData: FlBorderData(
+              show: false,
+            ),
+            sectionsSpace: 0,
+            centerSpaceRadius: 40,
+            sections: showingSections()),
+      ),
+    );
+  }
+
+  // combines smaller languages into an "other" language
+  List<LanguageResult> generateGraphLanguages(
+      ClocResult result, double threshold) {
+    bool addOther = false;
+    double remainingCode = 1.0;
+    List<LanguageResult> graphLanguages = [];
+    LanguageResult other = LanguageResult(
+      name: 'Other',
+      files: 0,
+      blank: 0,
+      comment: 0,
+      code: 0,
+      icon: LanguageIcon('Other'),
+    );
+
+    // check if remaining languages will create a slice lower than threshold
+    for (var language in result.languages) {
+      if (addOther ||
+          threshold > remainingCode - (language.code / result.totalCode)) {
+        other.files += language.files;
+        other.blank += language.blank;
+        other.comment += language.comment;
+        other.code += language.code;
+        addOther = true;
+      } else {
+        remainingCode -= language.code / result.totalCode;
+        graphLanguages.add(language);
+      }
+    }
+
+    // add other language slice if it contains languages
+    if (other.files + other.blank + other.comment + other.code > 0) {
+      graphLanguages.add(other);
+    }
+
+    return graphLanguages;
+  }
+
+  // generates pie chart data from graph languages
+  List<PieChartSectionData> showingSections() {
+    return List.generate(graphLanguages.length, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 16.0;
+      final radius = isTouched ? 120.0 : 100.0;
+      return PieChartSectionData(
+        color: graphColors[i],
+        value: graphLanguages[i].code.toDouble(),
+        title: graphLanguages[i].name,
+        radius: radius,
+        titleStyle: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xffffffff)),
+      );
+    });
   }
 }
